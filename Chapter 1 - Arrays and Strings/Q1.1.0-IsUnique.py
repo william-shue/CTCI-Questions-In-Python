717473880f78264a6324fd7d1213835680133ab1
William Shue <37516784+william-shue@users.noreply.github.com>
2019-12-11 09:09:11 -0500

    adding rest of the file . . .?

--- Chapter 1 - Arrays and Strings/Q1.1.0-IsUnique.py
+++ Chapter 1 - Arrays and Strings/Q1.1.0-IsUnique.py
@@ -16,12 +16,6 @@ def isUniqueChars(str):
     char_set[val] = True
   return True
 
-#some test examples:
-examples = ["abcdefg", "abcdefg123", "ABCDEFG", "ABCDEFG123", "!@#", "aabcdefg", "abcdefg1233"
-"DABCDEFFG", "2ABCDEFG123", "!#@#"]
-for s in examples:
-    print(isUniqueChars(s))
-
 '''
 In the case that we cannot use additional data structures, one method of determining uniqueness
 would be to simply compare each character in the string with every other, resuling in a O(n^2) runtime
@@ -31,4 +25,15 @@ def isUniqueChars(str):
     if len(str) > 128:
         return False
     for i in range(len(str)):
-        for j in range()
+        for j in range(len(str)):
+            if i == j:
+              continue
+            if str[i] == str[j]:
+                return False
+    return True
+
+#some test examples:
+examples = ["abcdefg", "abcdefg123", "ABCDEFG", "ABCDEFG123", "!@#", "aabcdefg", "abcdefg1233"
+"DABCDEFFG", "2ABCDEFG123", "!#@#"]
+for s in examples:
+    print(isUniqueChars(s))
