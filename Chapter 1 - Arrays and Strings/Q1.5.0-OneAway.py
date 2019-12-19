dc54b4dc67f623e67efba6e8bec99c71abd462ff
john-mpelkas <jwmpelkas@gmail.com>
2019-12-19 00:12:45 -0500

    File Set Up

---
+++ Chapter 1 - Arrays and Strings/Q1.5.0-OneAway.py
@@ -0,0 +1,19 @@
+"""There are three types of eddits that can be performed on a string:
+insert a character, remove a character, or replace a character.
+Given two strings, write a function to check if they are one edit
+(or zero edits) away."""
+
+#Examples:
+#Pale, Pie -> false
+#Pales, Pale -> true
+#Pales, Bale -> true
+#Pale, Bake -> false
+
+
+def strCompare():
+
+
+
+
+
+
