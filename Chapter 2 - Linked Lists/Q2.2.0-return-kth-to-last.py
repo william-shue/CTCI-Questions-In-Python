657ef2db36336e093714f2e11f5fb08ab1fb7cfb
William Shue <37516784+william-shue@users.noreply.github.com>
2020-01-26 18:28:56 -0500

    added prompt for return-kth-to-last

---
+++ Chapter 2 - Linked Lists/Q2.2.0-return-kth-to-last.py
@@ -0,0 +1,8 @@
+'''Implement an algorithm to find the kth
+to last element of a singly linked list.'''
+
+#will use the following def. of singly linked list
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
